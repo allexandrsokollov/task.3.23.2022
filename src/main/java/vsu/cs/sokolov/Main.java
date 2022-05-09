0be50c8611a78aa2deaf2fe61c78b92436c84790
Alexandr <allexandersokollov@gmail.com>
2022-05-09 17:19:44 +0300

    added readDataFromFile to Printer.java

--- src/main/java/vsu/cs/sokolov/Main.java
+++ src/main/java/vsu/cs/sokolov/Main.java
@@ -1,48 +1,15 @@
 package vsu.cs.sokolov;
 
 
-import vsu.cs.sokolov.entities.Appointment;
-import vsu.cs.sokolov.entities.FileHandler;
-import vsu.cs.sokolov.entities.FileReader;
-import vsu.cs.sokolov.entities.MyPriorityQueue;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
+import vsu.cs.sokolov.entities.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] fileData;
-        try {
-             fileData = FileReader.getFileData("C:\\Users\\Alexandr\\IdeaProjects\\task.3.23.2022\\appointments.txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Printer printer = new Printer();
+        printer.readDataFromFileToAdditionRow("C:\\Users\\Alexandr\\IdeaProjects\\task.3.23.2022\\appointments.txt");
 
-        FileHandler fileHandler = new FileHandler(fileData);
-        ArrayList<Appointment> appointments = fileHandler.getListOfAppointments();
-
-        Comparator<Appointment> comparator = (o1, o2) -> {
-
-            if (o1.getReceivingTime() < o2.getReceivingTime()) {
-                return 1;
-            } else if (o1.getReceivingTime() > o2.getReceivingTime()) {
-                return -1;
-            } else if (o1.getPriority() > o2.getPriority()) {
-                return 1;
-            } else if (o1.getPriority() < o2.getPriority()) {
-                return -1;
-            } else return Integer.compare(o1.getId(), o2.getId());
-        };
-
-        MyPriorityQueue<Appointment> priorityQueue = new MyPriorityQueue<>(comparator);
-
-        for (Appointment appointment : appointments) {
-            priorityQueue.add(appointment);
-        }
-
-        System.out.println(priorityQueue);
+        System.out.println(printer.getAdditionRow());
 
     }
 }
--- src/main/java/vsu/cs/sokolov/entities/Printer.java
+++ src/main/java/vsu/cs/sokolov/entities/Printer.java
@@ -1,4 +1,66 @@
 package vsu.cs.sokolov.entities;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Printer {
+    private int time;
+    private MyPriorityQueue<Appointment> additionRow;
+
+
+    public Printer() {
+        time = 0;
+
+        Runnable runnable = () -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                    time++;
+                    System.out.println(time);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+
+    public MyPriorityQueue<Appointment> getAdditionRow() {
+        return additionRow;
+    }
+
+    public void readDataFromFileToAdditionRow(String filepath) {
+        String[] fileData;
+        try {
+            fileData = FileReader.getFileData("C:\\Users\\Alexandr\\IdeaProjects\\task.3.23.2022\\appointments.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileHandler fileHandler = new FileHandler(fileData);
+        ArrayList<Appointment> appointments = fileHandler.getListOfAppointments();
+
+        Comparator<Appointment> comparator = (o1, o2) -> {
+
+            if (o1.getReceivingTime() < o2.getReceivingTime()) {
+                return 1;
+            } else if (o1.getReceivingTime() > o2.getReceivingTime()) {
+                return -1;
+            } else if (o1.getPriority() > o2.getPriority()) {
+                return 1;
+            } else if (o1.getPriority() < o2.getPriority()) {
+                return -1;
+            } else return Integer.compare(o1.getId(), o2.getId());
+        };
+
+        additionRow = new MyPriorityQueue<>(comparator);
+
+        for (Appointment appointment : appointments) {
+            additionRow.add(appointment);
+        }
+    }
+
 }
